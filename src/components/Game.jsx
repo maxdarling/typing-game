b9ede003513e35c417781b34d99a5793cba4b754
Paul-Elliot <peada@free.fr>
2025-11-11 20:48:29 +0100

    Stop at timer 0

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -14,12 +14,13 @@ const Game = () => {
   let [gameOver, setGameOver] = React.useState(false);
 
   function updateTimeLeft(inc) {
-      setTimeLeft((t) => {
-        if (t + inc <= 0) {
-          setGameOver(true);
-        }
-        return t + inc;
-      })
+    setTimeLeft((t) => {
+      if (t + inc <= 0) {
+        setGameOver(true);
+        return 0;
+      }
+      return t + inc;
+    });
   }
 
   React.useEffect(() => {
@@ -29,7 +30,7 @@ const Game = () => {
   function submitWord(word) {
     if (word[word.length - 1] === " ") {
       word = word.substring(0, word.length - 1);
-      const expected = nextWords[0]
+      const expected = nextWords[0];
       const correctness = word === expected;
       updateTimeLeft(correctness ? 1 : -1);
 
